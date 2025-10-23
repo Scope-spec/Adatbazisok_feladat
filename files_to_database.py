d575a140e5a72408c7a55a04da00551b550e8ebe
Scope-spec <davidpriegl@gmail.com>
2025-10-23 12:15:00 +0200

    Enhance data loading by specifying dtype and removing empty rows from CSV files

--- files_to_database.py
+++ files_to_database.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import sqlite3 as db
 
-df1 = pd.read_csv('Data_Files/Adagok.csv', sep=';')
-df2 = pd.read_csv('Data_Files/Hutopanelek.csv', sep=';')
+df1 = pd.read_csv('Data_Files/Adagok.csv', sep=';', dtype=str)
+df1 = df1.dropna(how='all')
+df2 = pd.read_csv('Data_Files/Hutopanelek.csv', sep=';', dtype=str)
+df2 = df2.dropna(how='all')
 
 conn = db.connect('Database/industrial.db')
 df1.to_sql('Adagok', conn, if_exists='replace', index=False)
