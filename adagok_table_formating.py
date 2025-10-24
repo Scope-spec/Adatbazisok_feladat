869a0db7f85e3e2dc3f3183b20c0e906019c3dc6
Scope-spec <davidpriegl@gmail.com>
2025-10-24 12:09:57 +0200

    Refactor datetime conversion and numeric type casting in Adagok table formatting script

--- adagok_table_formating.py
+++ adagok_table_formating.py
@@ -5,19 +5,23 @@ conn = db.connect('Database/industrial.db')
 df = pd.read_sql_query("SELECT * FROM Adagok", conn)
 cursor = conn.cursor()
 
-""""
-df['kezdet_datetime'] = pd.to_datetime(df['Kezdet_DATUM'].str.strip().str.replace('.', '-') + ' ' + df['Kezdet_IDO'].str.strip(),
-                                       errors='coerce', format='%Y-%m-%d %H:%M:%S')
-df['vege_datetime']   = pd.to_datetime(df['Vege_DATUM'].str.strip().str.replace('.', '-') + ' ' + df['Vege_IDO'].str.strip(),
-                                       errors='coerce', format='%Y-%m-%d %H:%M:%S')
-
+"""
+df['kezdet_datetime'] = df['Kezdet_DATUM'].str.strip().str.replace('.', '-') + ' ' + df['Kezdet_IDO'].str.strip()
+df['vege_datetime']   = df['Vege_DATUM'].str.strip().str.replace('.', '-') + ' ' + df['Vege_IDO'].str.strip()
 df.to_sql('Adagok', conn, if_exists='replace', index=False)
 """
+
 """
 cursor.execute('ALTER TABLE Adagok DROP COLUMN Kezdet_DATUM')
 cursor.execute('ALTER TABLE Adagok DROP COLUMN Kezdet_IDO')
 cursor.execute('ALTER TABLE Adagok DROP COLUMN Vege_DATUM')
 cursor.execute('ALTER TABLE Adagok DROP COLUMN Vege_IDO')
+"""
+
+"""
+df['ADAGIDO'] = pd.to_numeric(df['ADAGIDO'])
+df['ADAGSZAM'] = pd.to_numeric(df['ADAGSZAM'])
+df['ADAGKOZI IDO'] = pd.to_numeric(df['ADAGKOZI IDO'])
 """
 
 conn.commit()
