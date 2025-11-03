27f2ef049edfcda929f64b592f2792179a92d64b
Scope-spec <davidpriegl@gmail.com>
2025-11-03 12:42:33 +0100

    Refactor Hutopanelek and Adagok table formatting scripts by removing commented-out code and adding print statements for debugging

--- adagok_table_formating.py
+++ adagok_table_formating.py
@@ -5,24 +5,18 @@ conn = db.connect('Database/industrial.db')
 df = pd.read_sql_query("SELECT * FROM Adagok", conn)
 cursor = conn.cursor()
 
-"""
 df['kezdet_datetime'] = df['Kezdet_DATUM'].str.strip().str.replace('.', '-') + ' ' + df['Kezdet_IDO'].str.strip()
 df['vege_datetime']   = df['Vege_DATUM'].str.strip().str.replace('.', '-') + ' ' + df['Vege_IDO'].str.strip()
-df.to_sql('Adagok', conn, if_exists='replace', index=False)
-"""
 
-"""
 cursor.execute('ALTER TABLE Adagok DROP COLUMN Kezdet_DATUM')
 cursor.execute('ALTER TABLE Adagok DROP COLUMN Kezdet_IDO')
 cursor.execute('ALTER TABLE Adagok DROP COLUMN Vege_DATUM')
 cursor.execute('ALTER TABLE Adagok DROP COLUMN Vege_IDO')
-"""
 
-"""
 df['ADAGIDO'] = pd.to_numeric(df['ADAGIDO'])
 df['ADAGSZAM'] = pd.to_numeric(df['ADAGSZAM'])
 df['ADAGKOZI IDO'] = pd.to_numeric(df['ADAGKOZI IDO'])
+
 df.to_sql('Adagok', conn, if_exists='replace', index=False)
-"""
-conn.commit()
+print(df)
 conn.close()
